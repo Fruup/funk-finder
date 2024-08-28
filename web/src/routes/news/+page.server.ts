701d19716fdd213c1c78cb7c2ef9c0c470dd190e
Leon Scherer <info@leonscherer.com>
2024-08-28 19:02:44 +0200

    Fix image reloading
    - fix web build

--- web/src/routes/news/+page.server.ts
+++ web/src/routes/news/+page.server.ts
@@ -1,6 +1,8 @@
 import { getPocketbase } from '$lib/server/init'
 import type { News } from '@funk-finder/db/types/models'
 
+export const prerender = false
+
 export const load = async ({}) => {
 	const pb = await getPocketbase()
 	const news = await pb.collection<News<true>>('news').getFullList()
