17002c8e3a74d9afac086d4c7fdff2e7b8564a5c
Leon Scherer <info@leonscherer.com>
2024-09-24 00:48:54 +0200

    dev

--- scraper/src/job.ts
+++ scraper/src/job.ts
@@ -2,8 +2,19 @@ import { collectAll } from './collect'
 import { createEmbeddings } from './embedding'
 import { ocrAll } from './ocr'
 
-await collectAll()
-await ocrAll()
-await createEmbeddings()
+if (Bun.argv.includes('collect')) {
+	console.log('Collecting...')
+	await collectAll()
+}
+
+if (Bun.argv.includes('ocr')) {
+	console.log('OCR...')
+	await ocrAll()
+}
+
+if (Bun.argv.includes('embedding')) {
+	console.log('Embedding...')
+	await createEmbeddings()
+}
 
 console.info('Job done 🎉')
